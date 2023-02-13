8a835877e3840927f22524f50fa519f635b1ca7c
refract.admin <refract.admin>
2023-02-13 10:05:46 +0000

    auto save by knights-watch

--- TextFileWritingExample1.java
+++ TextFileWritingExample1.java
@@ -1,3 +1,26 @@
+public import java.io.FileWriter;
+import java.io.IOException;
+ 
+/**
+ * This program demonstrates how to write characters to a text file.
+ * @author www.codejava.net
+ *
+ */
 public class TextFileWritingExample1 {
+ 
+    public static void main(String[] args) {
+        try {
+            FileWriter writer = new FileWriter("/data/User_1_VSCode.txt", true);
+            writer.write("Hello World");
+            writer.write("\r\n");   // write new line
+            writer.write("Good morning!");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+ 
+    }
+ 
+}class TextFileWritingExample1 {
     
 }
